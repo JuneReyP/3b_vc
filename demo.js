87fc24aa1dea719aa5dea089403e49eb4423d10b
JuneReyP <junereypalabrica@gmail.com>
2023-10-10 11:29:50 +0800

    update

---
+++ demo.js
@@ -0,0 +1,99 @@
+// document.getElementById("demo").innerHTML = "Sample JS";
+
+// let docuID = document.getElementById("demo");
+
+// let value = innerHTML = "Sample JS";
+
+// document.write('write function');
+// alert('This is an alert!');
+
+
+
+
+// variables
+/* 
+var 
+let
+const
+*/
+var a = 5;
+let b =3;
+// let a = 'text';
+// console.log(a);
+const c = 4;
+
+// JavaScript Operators
+/* 
+Types of JavaScript Operators
+
+There are different types of JavaScript operators:
+
+    Arithmetic Operators 
+        Operator 	Description
+            + 	        Addition
+            - 	        Subtraction
+            * 	        Multiplication
+            ** 	        Exponentiation (ES2016)
+            / 	        Division
+            % 	        Modulus (Division Remainder)
+            ++ 	        Increment
+            -- 	        Decrement
+    Assignment Operators
+    Operator 	Example 	Same As
+        = 	    x = y 	    x = y
+        += 	    x += y 	    x = x + y
+        -= 	    x -= y 	    x = x - y
+        *= 	    x *= y 	    x = x * y
+        /= 	    x /= y 	    x = x / y
+        %= 	    x %= y 	    x = x % y
+        **= 	x **= y 	x = x ** y
+    Comparison Operators
+    Operator 	Description
+        == 	    equal to (1 == 1 or 1 == '11')
+        === 	equal value and equal type
+        != 	    not equal
+        !== 	not equal value or not equal type
+        > 	    greater than
+        < 	    less than
+        >= 	    greater than or equal to
+        <= 	    less than or equal to
+        ? 	    ternary operator
+    Logical Operators
+        Operator 	Description
+            && 	        logical and
+            || 	        logical or
+            ! 	        logical not
+
+            example
+            ((condition = T) || (condition = T))
+    Ternary Operators
+    ?
+    (condition) ? ifTrue : ifFalse;
+
+
+*/
+console.log(1 === '1');
+console.log(1 != '1');
+console.log(1 !== '1');
+
+/* 
+
+JavaScript has 8 Datatypes
+
+1. String
+2. Number
+3. Bigint
+4. Boolean
+5. Undefined
+6. Null
+8. Object
+
+The Object Datatype
+
+The object data type can contain:
+
+1. An object
+2. An array
+3. A date
+
+*/
